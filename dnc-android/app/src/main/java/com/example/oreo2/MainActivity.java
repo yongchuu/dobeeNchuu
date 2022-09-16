4b18b726e60b64f5c818f0051d70a3d80276d097
csWon <dobee0101@gmail.com>
2022-09-16 23:29:07 +0900

    htmlunit 시나리오 추가 안드로이드 시나리오 	막 서핑해서 만든 쿠키 setting 	네이버 메인 - www.naver.com 	키워드 검색 - https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query=허그체어+학생+메쉬 	쇼핑 클릭 - https://search.shopping.naver.com/search/all?where=all&frm=NVSCTAB&query=허그체어+학생+메쉬 	목적 게시물 클릭 			https://cr.shopping.naver.com/adcr.nhn?x=E6QFfv%2FUYMiZJDbiTOKZa%2F%2F%2F%2Fw%3D%3Dsotq%2Fzu%2FDB4WCs1H3CGkaNO4xSREZuYZ2WodnWLCEY9dBz1vuM2SOI7oibFli4j6TB%2Fh4nyf4W0boVT5npv%2BDtdr4LIQiYdxFziVUDc%2BiZ1sIAbztuhR26OIa22vl4jdBFrEj19yTrrJOs8nllN0%2Bqsc7NS5DH%2B%2BB0AnkucmYuzO7lcNxnWgwL8Q0owt3J7dmf9tprPlvE4kEDc%2FzhzDBRRENhMtcE%2B6r7RoGEohIfmX5wHP2qPLLPK3cmZXOPMRWw6ZjbtkbN7RD61dGUzg7G1qsmhWtsS26cBgiQoajA6qqWkQfXNpD2nBEcqIIKG23JS8AVWrm2%2FRtyPUiup%2Bx6cja5XIzAKN3O26KxyWVmNAAv2PkFiVSgPl9kuNpInbQxDV8eVY5PWFsmB4sA7yjsZ93EpgSMm4odyJu7HaFfCmNeqrFWKh%2FGVmgj6k5PLO5I8T7xUl%2FIAW3GNwslezDdZy6Cs%2BN4RObH8Z0RIvGtPALiM5oMty4B0COHADXy6PfHZyWBtTYLWsfHl%2B86pl486%2B6QpPjkV%2BwiJnQHbI1uIbwP2x4ZFBbrtL8oTn5x7%2BMkAlOKwPahCEyYd9E%2BlamjjnwuIA6tm%2Ff3ml3gW245wMJjxSqwKm39pf3hNiHBQEYzzDzwSPU74liSi8oO49JNR0cjwVm1BUYVknE7IEMpz4%3D&amp;nvMid=83676493016&amp;catId=50003299

--- dnc-android/app/src/main/java/com/example/oreo2/MainActivity.java
+++ dnc-android/app/src/main/java/com/example/oreo2/MainActivity.java
@@ -10,9 +10,11 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.gargoylesoftware.htmlunit.CookieManager;
 import com.gargoylesoftware.htmlunit.SilentCssErrorHandler;
 import com.gargoylesoftware.htmlunit.WebClient;
 
+import org.apache.commons.httpclient.Cookie;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Attributes;
 import org.jsoup.nodes.Document;
@@ -20,8 +22,12 @@ import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 import java.util.logging.Level;
@@ -78,34 +84,43 @@ public class MainActivity extends AppCompatActivity {
             new Thread(new Runnable() {
                 @Override
                 public void run() {
+                    List<String> urls = new ArrayList<>();
+                    urls.add("https://www.naver.com");
+                    urls.add("https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query=허그체어+학생+메쉬");
+                    urls.add("https://search.shopping.naver.com/search/all?where=all&frm=NVSCTAB&query=허그체어+학생+메쉬");
+                    urls.add("https://cr.shopping.naver.com/adcr.nhn?x=E6QFfv%2FUYMiZJDbiTOKZa%2F%2F%2F%2Fw%3D%3Dsotq%2Fzu%2FDB4WCs1H3CGkaNO4xSREZuYZ2WodnWLCEY9dBz1vuM2SOI7oibFli4j6TB%2Fh4nyf4W0boVT5npv%2BDtdr4LIQiYdxFziVUDc%2BiZ1sIAbztuhR26OIa22vl4jdBFrEj19yTrrJOs8nllN0%2Bqsc7NS5DH%2B%2BB0AnkucmYuzO7lcNxnWgwL8Q0owt3J7dmf9tprPlvE4kEDc%2FzhzDBRRENhMtcE%2B6r7RoGEohIfmX5wHP2qPLLPK3cmZXOPMRWw6ZjbtkbN7RD61dGUzg7G1qsmhWtsS26cBgiQoajA6qqWkQfXNpD2nBEcqIIKG23JS8AVWrm2%2FRtyPUiup%2Bx6cja5XIzAKN3O26KxyWVmNAAv2PkFiVSgPl9kuNpInbQxDV8eVY5PWFsmB4sA7yjsZ93EpgSMm4odyJu7HaFfCmNeqrFWKh%2FGVmgj6k5PLO5I8T7xUl%2FIAW3GNwslezDdZy6Cs%2BN4RObH8Z0RIvGtPALiM5oMty4B0COHADXy6PfHZyWBtTYLWsfHl%2B86pl486%2B6QpPjkV%2BwiJnQHbI1uIbwP2x4ZFBbrtL8oTn5x7%2BMkAlOKwPahCEyYd9E%2BlamjjnwuIA6tm%2Ff3ml3gW245wMJjxSqwKm39pf3hNiHBQEYzzDzwSPU74liSi8oO49JNR0cjwVm1BUYVknE7IEMpz4%3D&amp;nvMid=83676493016&amp;catId=50003299");
 
-                    String url = "https://blog.naver.com/dobee0101/222222434066";
-                    String url2 = "https://cswon.tistory.com/entry/%EC%96%91%EC%9E%AC%EC%97%AD-Wine-Bar-Rhyme";
-//                    String url = "https://www.anycodings.com/questions/navigating-to-javascript-form-using-htmlunit";
-//                    String url = "https://www.naver.com";     // Pass
-//                    String url = "https://www.tistory.com";     // Pass
                     WebClient webClient = new WebClient();
-
-//                  webClient.getOptions().setThrowExceptionOnScriptError(false);
-                    //webClient.getOptions().setJavaScriptEnabled(true);
-
-                    webClient.setThrowExceptionOnScriptError(false);
+                    webClient.setRedirectEnabled(true);
                     webClient.setJavaScriptEnabled(true);
+                    webClient.setThrowExceptionOnFailingStatusCode(false);
+                    webClient.setThrowExceptionOnScriptError(false);
+                    webClient.setCssEnabled(false);
 
-                    GetTestProp().forEach((k,v)->{
-                        webClient.addRequestHeader(k,v);
+//                    webClient.setThrowExceptionOnScriptError(false);
+//                    webClient.setCssEnabled(false);
+//                    webClient.setJavaScriptEnabled(true);
+
+//                    CookieManager cm = webClient.getCookieManager();
+//                    Cookie cookie = new Cookie();
+//                    cm.addCookie(cookie);
+
+//                    GetTestProp2().forEach((k,v)->{
+//                        webClient.addRequestHeader(k,v);
+//                    });
+
+                    urls.forEach(url->{
+                        HtmlPage htmlPage=null;
+                        try {
+                            htmlPage = webClient.getPage(url);
+                            printCookieln(htmlPage.getTitleText());
+
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                            printCookieln(e.toString());
+                        }
                     });
 
-                    Logger.getLogger("com.gargoylesoftware.htmlunit.javascript.host.css").setLevel(Level.SEVERE);
-                    HtmlPage htmlPage = null;
-                    try {
-                        htmlPage = webClient.getPage(url2);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                        printCookieln(e.toString());
-                    }
-                    String html = htmlPage.getTitleText();
-                    printCookieln(html);
                 }
             }).start();
         });
@@ -156,17 +171,18 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(view -> {
             // final String url_1 = "https://blog.naver.com/dduchelin/222042185772";
             // final String url_1 = "https://blog.naver.com/dduchelin/222031115434";   //제레미
-            final String url_1 = "https://blog.naver.com/dobee0101/222222434066";
+            final String url_1 = "https://search.shopping.naver.com/search/all?where=all&frm=NVSCTAB&query=허그체어+컴퓨터";
+//            final String url_1 = "https://blog.naver.com/dobee0101/222222434066";
             final String url_2 = "https://www.google.com";
             final String url_3 = "https://cswon.tistory.com/entry/%EC%9C%84%EB%A1%80-%ED%94%BC%EC%9E%90-%ED%8C%8C%EC%8A%A4%ED%83%80-%EB%A7%9B%EC%A7%91-%EB%9D%BC%EB%B2%A8%EB%A1%9CRavello";
 
-            Map<String, String> reqProp_1 = GetTestProp();
+            //Map<String, String> reqProp_1 = GetTestProp();
+            Map<String, String> reqProp_1 = new HashMap<>();
             Log.i("Tag", "!!!start!!!");
 
             new Thread(new Runnable(){
                 @Override
                 public void run(){
-
                     AssetManager am = getResources().getAssets() ;
                     InputStream is = null ;
 
@@ -234,7 +250,6 @@ public class MainActivity extends AppCompatActivity {
 
                         Object p[] =  new Object[] {"script"};
                         je.callFunction("getElementsByTagName", p);
-
 
 
 
@@ -356,6 +371,25 @@ public class MainActivity extends AppCompatActivity {
             }).start();
         });
     }
+
+    public HtmlPage getHtmlUnit(String url, WebClient webClient)
+    {
+        HtmlPage htmlPage=null;
+//        String searchUrl = "https://search.naver.com/search.naver?where=nexearch&sm=tab_jum&query=허그체어+컴퓨터+공부";
+        try {
+            htmlPage = webClient.getPage(url);
+//            webClient.getCookieManager();
+            printCookieln(htmlPage.getTitleText());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            printCookieln(e.toString());
+        }
+        return htmlPage;
+    }
+
+
+
     public String GetJsEnvStr(){
         String retStr = null;
         InputStream is;
@@ -373,6 +407,25 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace() ;
         }
         return retStr;
+    }
+
+    public Map<String, String> GetTestProp2()
+    {
+        String cookieStr = "NNB=YZPA4MWBFADGG; nx_ssl=2; SB_MODE=plusdeal; ASID=3d62cccb000001830e0078bf00016baf; JSESSIONID=FD836B497F38DE32EFF37150EC104AB4.jvm1; CBI_SES=PjT6fCsrasr6o0Tm/i49DWWsqxMAazREpUH2py5bnWfHReF5dhqCUVuFHfP8ZU0CZqcZ8fJSEP2btONPW1+ePshgQpzNw1wraf73LL9GRU8ZjHwly3eX4N8xiB6xxaMkqDp04QatlQjxQLZQ+mtbEO4BdvOhbLFKT1sXIVajgTiCHmXAqLv7UP5sN8R/E3R9u/IQFDsq/atwpbQ65LnOpykoaQwotsjMgP7lVLzIelA2lqfRLCkrPOkah3+tJD7MF3DCAlT/9MbKlmpRoD4IRW8ChsmJlNxhjSsZhn6D7tYpbpEageFTo7mbOpl+7pwWDXHWW5Bitv59R0XN8vzStLFUoi+khUsFf8w1ypHSwecw8X9Lr/Ecl33Fvo4+7jdMQ+WSKkz4BizRCWjCPMcXy8R6/JkDnlW1rwkRq8Yh3HVXkRRjcqbrPm8lGLGAZhcN; NFS=2; NM_TOP_PROMOTION=1; CBI_CHK=\"r5V0mf9uRUZHZ/vmLGy3ez7f4/k4aqWXL5o03eN68fqwOAwUjJMs8y7llUhljjULi3OhZcTCmq4mzUdgCHVl5GwIw9A9j8mbaJhee3UkrWUbdAmCpO0LmzdZrIWxv0dshMIs5zEfaVYvWyYVCkSNnNC0DYvMUGi21BhCYBK7Fd4=\"; nid_inf=1393432957; NID_AUT=xBiSHiMeoP7NUFKwO00VARKjIHiPbgRFSdIhizqX+kq0S3MmnL4IYnslwMyncihq; NID_JKL=Ks3wcANZ0wQvbHHZ0I/8LScyNkYzPBP5j21GOJliGng=; page_uid=hyeonwp0J1sssdIElMZssssstGG-034477; PM_CK_loc=f565d4a6f8b5ae26636fa1a2d206d77e1de1ead5af690fd24ee6c2a240a944db; NID_SES=AAABhDS49Ogs16vQSY3jC9ePIDPdZKmpXKmLW34wHY3ye3BEPtFjt8JX2mj8f6jkJ8ygr3Su7GwyZ69U4EBjo77f9B0vurFlCxDTFPHdZHF4H+Iw6xPfsf4cAyh+wg+zt3HnpBw0NlxdgqSfoMYewKhqi46w76b0POHYUuD/t1/1DF/q8mbTcTBWlCJqkOwKcmmBkoV1ro6NnwipnP2orb0MBp78UYmmEPIm8BAOO5FfxDULTDMdqHahKOWU4SddnpktehGmKb6RuFk3cKgUprm1VRdrNvg171ETl+JINd/YpXh5b7uFKDeUMQcta+tSDwNg5ij1lXJc0cxT7iS7BBJ+ZWUsuIORMYdS75a6VD1VMmd+eTQrrlHFC13iRgAHrk8ATKdHARR/63WoCdcSrcZZ+Hfw30XGbQHz9IkfZQncPu2eJgcrSDRxgfJCFle/W7PUWdWcs0vcmOQV7No2vqFniY0eyKWZtOfoHJSK/Hz6YAPj3nhDPu/3XgsRERZvS3y/dqxHo37aEQDvVT/q8BVRbRU=; BMR=s=1663336568178&r=https://news.naver.com/&r2=https://www.naver.com/";
+        
+        String[] cookieKV = cookieStr.split("; ");
+
+        Map<String, String> reqProp_1 = new HashMap<>();
+        for(String kv :cookieKV)
+        {
+            String[] _kv = kv.split("=");
+            if(_kv.length==2)
+                reqProp_1.put(_kv[0],_kv[1]);
+            else
+                reqProp_1.put(_kv[0],"");
+        }
+
+        return reqProp_1;
     }
 
     public Map<String, String> GetTestProp()
