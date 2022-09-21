5e4d5c09675e1266eeb23002f8b3c39f05d2ebc2
dongjoon ju <yongchuu@naver.com>
2022-09-22 02:02:53 +0900

    merge

--- dnc-crawler/app/src/main/java/com/dnc/crawler/ui/home/HomeFragment.java
+++ dnc-crawler/app/src/main/java/com/dnc/crawler/ui/home/HomeFragment.java
@@ -1,5 +1,9 @@
 package com.dnc.crawler.ui.home;
 
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
